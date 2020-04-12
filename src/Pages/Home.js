41f80c142b918a3215d3d3039072fc29b968e864
Paliy Stepan <palstepweb@gmail.com>
2020-04-12 13:13:46 +0300

    add loader component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React, {Fragment, useContext} from 'react';
 import {Search} from '../components/Search';
 import {Card} from '../components/Card';
 import {GithubContext} from '../context/github/githubContext';
+import Loader from "../components/Loader";
 
 export const Home = () => {
   const {loading, users} = useContext(GithubContext);
@@ -10,18 +11,15 @@ export const Home = () => {
   return (
     <Fragment>
       <Search />
-
       <div className="row">
-
         {loading
-          ? <div className="col-12"><p className="text-center">Загрузка...</p></div>
+          ? <div className="col-12 text-center">  <Loader /> </div>
           : users.map(user => (
             <div className="col-md-6 col-lg-4 mb-4" key={user.id}>
               <Card user={user} />
             </div>
           ))
         }
-
       </div>
 
     </Fragment>
---
+++ src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Loader = () => (
+    <div className="spinner-grow text-info text-center loader" role="status">
+    </div>
+);
+
+export default Loader
